3592c2391ef78df7e4b5cec7ca2c1f4edf067614
Leandro Dantas <leandrosuy@gmail.com>
2021-09-29 17:58:54 -0300

    Feature delete

--- Routes/index.js
+++ Routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const Users = require('../model/filmes')
+const Filmes = require('../model/filmes')
 
 //  Rota de GET
 router.get('/filmes', (req, res) => {
-    Users.find({}, (err, data) => {
+    Filmes.find({}, (err, data) => {
         if(err) return res.send({ message: 'Erro na consulta dos filmes'});
         return res.send(data);
     });
@@ -15,15 +15,22 @@ router.post('/criarfilme', (req, res) => {
     const {id, nome, capa, descricao} = req.body;
     if(!id || !nome || !capa || !descricao) return res.send({ error: "Dados insuficientes!" });
 
-    Users.findOne({ id }, (err, data) => {
+    Filmes.findOne({ id }, (err, data) => {
         if(err) return res.send({ error: "Erro ao buscar filmes"});
         if(data) return res.send({ error: "Filme já cadastrado"});
 
-        Users.create(req.body, (err, data) => {
+        Filmes.create(req.body, (err, data) => {
             if(err) return res.send({ error: "Erro ao criar Filme!"});
             return res.send(data);
         });
     });
 });
+
+router.delete('/excluirfilme/:id', (req, res) => {
+    const filme = Filmes.deleteOne({_id: req.params.id}, (err) => {
+        if(err) return res.send({ error: "Erro ao excluir o Filme!" })
+    });
+    return res.send({ message: 'Filme excluido com sucesso!'})
+})
 
 module.exports = router;
